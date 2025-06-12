b3242992c2abf27523878b39adffe9f6c7399b90
HaccerKat <jiapengbai@gmail.com>
2025-06-11 17:57:53 -0700

    added more dataplots for analysis purposes

--- alphazero_nnue/training_helper.py
+++ alphazero_nnue/training_helper.py
@@ -3,10 +3,10 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 def loss_fn(prediction, target):
     # could play around with the proportions later
     # policy
-    loss = torch.sum(-target[0] * torch.nn.functional.log_softmax(prediction[0], dim=1), dim=1).mean()
+    policy_loss = torch.sum(-target[0] * torch.nn.functional.log_softmax(prediction[0], dim=1), dim=1).mean()
     # value
-    loss += torch.nn.functional.mse_loss(prediction[1], target[1])
-    return loss
+    value_loss = torch.nn.functional.mse_loss(prediction[1], target[1])
+    return policy_loss, value_loss
 
 def train_loop(dataloader, model, optimizer, BATCH_SIZE):
     size = len(dataloader.dataset)
@@ -15,26 +15,39 @@ def train_loop(dataloader, model, optimizer, BATCH_SIZE):
     for batch, (input, policy, value) in enumerate(dataloader):
         input, policy, value = input.to(device), policy.to(device), value.to(device)
         prediction = model(input)
-        loss = loss_fn(prediction, (policy, value))
+        policy_loss, value_loss = loss_fn(prediction, (policy, value))
+        loss = policy_loss + value_loss
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         if (batch + 1) % fifths == 0:
             loss, current = loss.item(), batch * BATCH_SIZE
-            print(f"loss: {loss:>7f} [{current:>5d}|{size:>5d}]")
+            policy_loss, value_loss = policy_loss.item(), value_loss.item()
+            print(f"Loss: {loss:>7f} [{current:>5d}|{size:>5d}]")
+            print(f"Policy Loss: {policy_loss:>7f}")
+            print(f"Value Loss: {value_loss:>7f}")
 
 
 def test_loop(dataloader, model):
     model.eval()
     num_batches = len(dataloader)
-    test_loss = 0
+    policy_loss, value_loss = 0, 0
     with torch.no_grad():
         for input, policy, value in dataloader:
             input, policy, value = input.to(device), policy.to(device), value.to(device)
             prediction = model(input)
-            test_loss += loss_fn(prediction, (policy, value)).item()
+            add_policy_loss, add_value_loss = loss_fn(prediction, (policy, value))
+            add_policy_loss = add_policy_loss.item()
+            add_value_loss = add_value_loss.item()
+            policy_loss += add_policy_loss
+            value_loss += add_value_loss
 
-    test_loss /= num_batches
+    policy_loss /= num_batches
+    value_loss /= num_batches
+    test_loss = policy_loss + value_loss
     # print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg Loss: {test_loss:>8f} \n")
-    print(f"Avg Loss: {test_loss:>8f} \n")
-    return test_loss
+    print("----------------------------------------------------------------")
+    print(f"Avg Loss: {test_loss:>8f}")
+    print(f"Policy Loss: {policy_loss:>8f}")
+    print(f"Value Loss: {value_loss:>8f} \n")
+    return policy_loss, value_loss, test_loss
